2c2e1df16cb78da15ef2196bfbcc10937aca2da0
Ryu <107185709+yujewook@users.noreply.github.com>
2024-03-13 12:51:24 +0900

    Add files via upload

---
+++ JavaPratice/src/Baekjoon/baekjoon_15552.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String args[]) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int T = Integer.parseInt(br.readLine());
+
+        StringTokenizer st;
+
+        for(int i=0; i<T; i++){
+            st = new StringTokenizer(br.readLine()," ");
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            bw.write((N + M) + "\n");
+        }
+        br.close();
+
+        bw.flush();     // 남아있는 데이터 모두 출력
+        bw.close();
+
+    }
+}
