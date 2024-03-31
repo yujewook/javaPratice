db8b7dbada2d75b7005268071e84cd45ea43e766
Ryu <107185709+yujewook@users.noreply.github.com>
2024-03-31 16:39:43 +0900

    Add files via upload

---
+++ JavaPratice/src/Baekjoon/Baekjoon_2743.java
@@ -0,0 +1,17 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_2743 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String input =sc.next();
+		sc.close();
+		System.out.print(input.length());
+		
+
+	}
+
+}
---
+++ JavaPratice/src/Baekjoon/Baekjoon_27866.java
@@ -0,0 +1,16 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_27866 {
+
+	public static void main(String[] args) {
+	Scanner sc = new Scanner(System.in);
+	
+	String S = sc.next();
+	int i = sc.nextInt();
+	System.out.print(S.charAt(i-1));
+	
+	}
+
+}
