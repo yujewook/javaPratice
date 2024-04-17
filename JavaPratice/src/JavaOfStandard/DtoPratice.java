453667316ab505b9206a7ff3c5ce54f033e16f84
Ryu <107185709+yujewook@users.noreply.github.com>
2024-04-18 00:11:06 +0900

    Add files via upload

--- JavaPratice/src/JavaOfStandard/DtoPratice.java
+++ JavaPratice/src/JavaOfStandard/DtoPratice.java
@@ -4,12 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DtoPratice {
+public class DtoPraticeList {
+	List<DtoPratice> DtoPraticeList = new ArrayList<DtoPratice>();
+	public List<DtoPratice> getDtoPraticeList() {
+		return DtoPraticeList;
+	}
+
+	public void setDtoPraticeList(List<DtoPratice> dtoPraticeList) {
+		DtoPraticeList = dtoPraticeList;
+	}
+	}
+
 private int a ;
+
+public int getA() {
+	return a;
+}
+
+public void setA(int a) {
+	this.a = a;
+}
+
+public int getB() {
+	return b;
+}
+
+public void setB(int b) {
+	this.b = b;
+}
+
 private int b ;
 
-List<DtoPratice> DtoPraticeList = new ArrayList<DtoPratice>();
+
 }
 
 class outDto{
-	
+	List<DtoPratice.DtoPraticeList> outDat = null;
 }
