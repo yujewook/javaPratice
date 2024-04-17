7f5a6767c84ad75d968a4368a1525e3131806942
Ryu <107185709+yujewook@users.noreply.github.com>
2024-04-17 23:54:15 +0900

    Add files via upload
    Dto연습 List형

---
+++ JavaPratice/src/JavaOfStandard/DtoPratice.java
@@ -0,0 +1,15 @@
+package JavaOfStandard;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DtoPratice {
+private int a ;
+private int b ;
+
+List<DtoPratice> DtoPraticeList = new ArrayList<DtoPratice>();
+}
+
+class outDto{
+	
+}
