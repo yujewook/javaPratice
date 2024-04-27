c2cf4c2d6afb6eafbbf168effb650a54bc0d7e53
Ryu <107185709+yujewook@users.noreply.github.com>
2024-04-27 22:24:25 +0900

    Add files via upload

---
+++ JavaPratice/src/Baekjoon/Baekjoon_10798.java
@@ -0,0 +1,25 @@
+package Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_10798 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String [] array = new String[10];
+		for(int i = 0 ; i < array.length ;i++) {
+			String input =sc.nextLine();
+			array[i] = input;
+		}
+		sc.close();	
+		
+		for(int i = 0 ; i < array.length ;i++) {
+	
+		}
+			
+		
+		
+
+	}
+
+}
