b4d36acb113658035e4597c925951a3d1858b152
Ryu <107185709+yujewook@users.noreply.github.com>
2024-04-28 10:04:29 +0900

    Add files via upload

--- JavaPratice/src/Baekjoon/Baekjoon_10798.java
+++ JavaPratice/src/Baekjoon/Baekjoon_10798.java
@@ -42,3 +42,49 @@ public class Baekjoon_10798 {
     }
 
 }
+/*package Baekjoon;
+
+import java.util.Scanner;
+
+public class Baekjoon_10798 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String [] array = new String[3];
+		for(int i = 0 ; i < array.length ;i++) {
+			String input =sc.nextLine();
+			array[i] = input;
+		}
+		sc.close();	
+		int i =0;
+		
+		StringBuilder sb = new StringBuilder();
+		int maxLength = 0;
+		
+		//단어의 숫자
+		while(i < array.length ) {
+			
+			String output = array[i];
+	        
+			//단어안의 문자갯수
+			for (int j = 0 ; j < output.length() ; j++) {
+				char insertChar  = output.charAt(j);
+				if(i == 0 ) {
+				  sb.insert( j+i , insertChar);
+				} else {
+				  	
+				}
+				
+			}
+			System.out.println("중간 "+ sb.toString());
+			i++;
+		}
+		
+		System.out.println(sb.toString());
+		
+		
+
+	}
+
+}
+*/
