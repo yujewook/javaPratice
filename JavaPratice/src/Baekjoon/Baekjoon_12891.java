d583728ca9c3a2bd704b3daff29cec2f8d711b9d
Ryu <jaewook517@naver.com>
2024-10-23 23:48:36 +0900

    아직 강의 중간//

--- JavaPratice/src/Baekjoon/Baekjoon_12891.java
+++ JavaPratice/src/Baekjoon/Baekjoon_12891.java
@@ -1,10 +1,35 @@
 package Baekjoon;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Baekjoon_12891 {
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader bf =new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer strToken = new StringTokenizer(bf.readLine());
+		int S = Integer.parseInt(strToken.nextToken()); //전체 문자열 길이
+		int P = Integer.parseInt(strToken.nextToken());
+		
+		int Result = 0;
+		int checkArr [] = new int[4];//비밀번호 상태
+		int myArr [] = new int[4]; //현재 상태 배열
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		char A[] = new char[S];
+		int chechSecret = 0;
+		
+		A = bf.readLine().toCharArray();
+		strToken = new StringTokenizer(bf.readLine());//새로운 문자배열
+		for (int i = 0 ; i < 4 ; i++) {
+			checkArr[i] = Integer.parseInt(strToken.nextToken());
+			
+			if( checkArr[i] == 0 ) {
+				chechSecret++;
+			}
+		}
+		
 	}
 
 }
