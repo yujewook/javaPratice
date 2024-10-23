876f0669d079ea26593e3778e39e18220e4b3f0c
Ryu <jaewook517@naver.com>
2024-10-23 23:48:52 +0900

    중간..

--- JavaPratice/src/Baekjoon/Baekjoon_12891.java
+++ JavaPratice/src/Baekjoon/Baekjoon_12891.java
@@ -14,11 +14,11 @@ public class Baekjoon_12891 {
 		int P = Integer.parseInt(strToken.nextToken());
 		
 		int Result = 0;
-		int checkArr [] = new int[4];//비밀번호 상태
+		int checkArr [] = new int[4];//비밀번호 조건
 		int myArr [] = new int[4]; //현재 상태 배열
 
 		char A[] = new char[S];
-		int chechSecret = 0;
+		int chechSecret = 0; //비밀번호 조건이 맞는경우 
 		
 		A = bf.readLine().toCharArray();
 		strToken = new StringTokenizer(bf.readLine());//새로운 문자배열
