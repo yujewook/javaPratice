f62de5dd92ebc8af19b56738269c80ea92bd7b9a
Ryu <jaewook517@naver.com>
2024-11-11 23:43:36 +0900

    독하게 자바

---
+++ JavaPratice/src/java_harshly/ChaInt.java
@@ -0,0 +1,12 @@
+package java_harshly;
+
+public class ChaInt {
+
+	public static void main(String[] args) {
+		System.out.println('A');
+		System.out.println((char) (65+1) );
+		System.out.println((char) ('A'+2));
+
+	}
+
+}
