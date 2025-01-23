9edca5108939a8729fb323b004b33a28d0b2a188
Ryu <jaewook517@naver.com>
2025-01-23 23:24:09 +0900

    정답_

--- JavaPratice/src/Baekjoon/Baekjoon_12891.java
+++ JavaPratice/src/Baekjoon/Baekjoon_12891.java
@@ -5,31 +5,71 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
-public class Baekjoon_12891 {
+public class Baekjoon_12891 {	static int[] check;
+public static void main(String[] args) throws IOException{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	StringTokenizer st = new StringTokenizer(br.readLine());
 	
-	public static void main(String[] args) throws IOException {
-		BufferedReader bf =new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer strToken = new StringTokenizer(bf.readLine());
-		int S = Integer.parseInt(strToken.nextToken()); //전체 문자열 길이
-		int P = Integer.parseInt(strToken.nextToken());
-		
-		int Result = 0;
-		int checkArr [] = new int[4];//비밀번호 조건
-		int myArr [] = new int[4]; //현재 상태 배열
-
-		char A[] = new char[S];
-		int chechSecret = 0; //비밀번호 조건이 맞는경우 
-		
-		A = bf.readLine().toCharArray();
-		strToken = new StringTokenizer(bf.readLine());//새로운 문자배열
-		for (int i = 0 ; i < 4 ; i++) {
-			checkArr[i] = Integer.parseInt(strToken.nextToken());
-			
-			if( checkArr[i] == 0 ) {
-				chechSecret++;
-			}
-		}
-		
+	int S = Integer.parseInt(st.nextToken());
+	int P = Integer.parseInt(st.nextToken());
+	
+	String str = br.readLine();
+	char[] pw = new char[S];
+	int[] dnaNum = new int[4];
+	int[] currNum = new int[4];
+	check = new int[4];
+	int result=0;
+	
+	for(int i=0;i<S;i++) { //몇 개 있는지 체크
+		pw[i] = str.charAt(i);
+		if(pw[i] == 'A') dnaNum[0]++;
+		if(pw[i]=='C') dnaNum[1]++;
+		if(pw[i]=='G') dnaNum[2]++;
+		if(pw[i]=='T') dnaNum[3]++;
 	}
+	
+	st = new StringTokenizer(br.readLine());
+	for(int i=0;i<4;i++) {//최소로 들어가야하는 개수
+		check[i] = Integer.parseInt(st.nextToken());
+	}
+	
+	if(!checkNum(dnaNum)) {
+		System.out.print(result);
+		return;
+	}
+	
+	for(int i=0;i<P;i++) { //슬라이딩하기 위한 초기화 과정
+		if(pw[i] == 'A') currNum[0]++;
+		if(pw[i]=='C') currNum[1]++;
+		if(pw[i]=='G') currNum[2]++;
+		if(pw[i]=='T') currNum[3]++;
+	}
+	if(checkNum(currNum)) result++;
+	
+	for(int j=P;j<S;j++) { //j는 뒷부분 인덱스
+		int i=j-P; //i는 앞부분 인덱스
+		
+		if(pw[i]=='A') currNum[0]--;
+		if(pw[i]=='C') currNum[1]--;
+		if(pw[i]=='G') currNum[2]--;
+		if(pw[i]=='T') currNum[3]--;
+		
+		if(pw[j]=='A') currNum[0]++;
+		if(pw[j]=='C') currNum[1]++;
+		if(pw[j]=='G') currNum[2]++;
+		if(pw[j]=='T') currNum[3]++;
+		
+		if(checkNum(currNum)) result++;
+	}
+	
+	System.out.print(result);
+}
 
+public static boolean checkNum(int[] num) {
+	for(int i=0;i<4;i++) {
+		if(num[i] < check[i])
+			return false;
+	}
+	return true;
+}
 }
