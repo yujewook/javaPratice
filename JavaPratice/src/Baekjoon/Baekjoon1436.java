b84894e6ecc9b52ff502fe0f6b0df59a755203aa
Ryu <jaewook517@naver.com>
2025-04-28 11:13:41 +0900

    오늘 문제

--- JavaPratice/src/Baekjoon/Baekjoon1436.java
+++ JavaPratice/src/Baekjoon/Baekjoon1436.java
@@ -3,53 +3,44 @@ package Baekjoon;
 import java.util.Scanner;
 
 /*
-문제: https://www.acmicpc.net/problem/7568
-문제 제목: 백준 7568번 - 덩치
-
-문제 요약:
-몸무게와 키를 입력받아, 자신보다 덩치가 큰 사람이 몇 명인지 세고,
-등수를 출력하는 문제. 몸무게와 키 모두 작아야 덩치가 작다고 판단한다.
-.
-
-
-예제 입력:
-5
-55 185
-58 183
-88 186
-60 175
-46 155
-
-예제 출력:
- 2 2 1 2 5
-*/
+ * 문제: 백준 1436번 - 영화감독 숌
+ * 링크: https://www.acmicpc.net/problem/1436
+ *
+ * 문제 요약:
+ * - "666"이 들어가는 수들을 작은 순서대로 나열했을 때
+ * - N번째에 오는 수를 출력한다.
+ *
+ * 입력:
+ * - 첫째 줄: 정수 N (1 ≤ N ≤ 10,000)
+ *
+ * 출력:
+ * - N번째 종말의 수를 출력
+ *
+ * 예시 입력:
+ * 2
+ *
+ * 예시 출력:
+ * 1666
+ */
 public class Baekjoon1436 {
-	
-	public static void main(String[] args) {
+
+    public static void main(String[] args) {
+        // 여기에 코드 작성하면 돼
     	Scanner sc = new Scanner(System.in);
-    	int[][] people = {
-			    		    {55, 185},
-			    		    {58, 183},
-			    		    {88, 186},
-			    		    {60, 175},
-			    		    {46, 155} };
-    	
-    	for(int i = 0 ; i < people.length ; i++ ) {
-    		int rank = 1;
-    		for(int j = 0 ; j < people.length ; j++ ) {
-    			if( i == j ) {
-    				continue;
+    	int rank = 2; // sc.nextInt();
+    	String num = "666";
+    	int flag = 0;
+    	int a = 0;
+    	for (int N = 1 ; N < 10001 ; N++) {
+ 
+    		String ex = String.valueOf(N);
+    		if (num.contentEquals(ex)) {
+    			flag++;
+    			if(flag == rank) {
+    				break;
     			}
-    			if(people[i][0] < people[j][0] && people[i][1] < people[j][1]) {
-    				rank++;
-    			}
-    				
-    		}
-    		System.out.print(rank + " ");
+    		};
     	}
-    	
-	}
-	
+    }
+
 }
-
-
